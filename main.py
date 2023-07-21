0e48dfc170498de0ac8b91bf66972127679e9a5d
Feersys <workeer@mail.ru>
2023-07-21 17:12:28 +0300

    handlers done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
-
+from flask_api import status
 
 app = Flask(__name__)
 h = "Hello"
-users = [{"id": 1, "name": "Abraham", "sname": "Lincoln"},
-         {"id": 2, "name": "Donald", "sname": "Trump"},
-         {"id": 3, "name": "Donald", "sname": "Mc.Duck"}]
+error = "Sorry, not found"
+users = [{"id": 0, "name": "Abraham", "sname": "Lincoln"},
+         {"id": 1, "name": "Donald", "sname": "Trump"},
+         {"id": 2, "name": "Donald", "sname": "Mc.Duck"}]
 query_params = {"id": 1}
 
 
@@ -21,23 +22,37 @@ def post():
     return str(content) + "!!!"
 
 
-@app.route("/users", methods=['GET'], params=query_params)
-def users_list():
-    return jsonify(users)
+@app.route("/users", methods=['GET'])
+def users_list_query():
+    user_id = request.args.get('id')
+    user_id = int(user_id)
+    if user_id > len(users)-1:
+        return error, 404
+    user = users[user_id]
+    return jsonify(user)
+
+
+@app.route("/users/<user_id>", methods=['GET'])
+def users_list_route(user_id):
+    user_id = int(user_id)
+    if user_id > len(users) - 1:
+        return error, 404
+    return jsonify(users[user_id])
 
 
 @app.route('/endpoint', methods=['DELETE'])
 def delete_endpoint():
-    return jsonify({'error': 'I\'m a teapot'}), 418
+    return jsonify({'error': 'I\'m a teapot'}), status.HTTP_418_IM_A_TEAPOT
 
 
 @app.route('/change', methods=['PUT'])
 def change():
-    content = str(request.data.decode("utf-8"))+" input text"
+    content = {}
+    text = str(request.data.decode("utf-8"))
+    content["input text"] = text
     print(content)
     return jsonify(content)
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
