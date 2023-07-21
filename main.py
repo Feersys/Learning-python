4efd0db4cfea0cd04a2aa68f609d762360feb07f
Feersys <workeer@mail.ru>
2023-07-21 13:24:13 +0300

    include ping handler etc

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from flask import Flask
-import requests
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
+h = "Hello"
+users = [{"id": 1, "name": "Abraham", "sname": "Lincoln"},
+         {"id": 2, "name": "Donald", "sname": "Trump"},
+         {"id": 3, "name": "Donald", "sname": "Mc.Duck"}]
+query_params = {"id": 1}
 
 
-@app.route("/ping")
-def index():
-    return "Hello!"
+@app.route("/")
+def main():
+    return h
+
+
+@app.route("/answer", methods=['POST'])
+def post():
+    content = request.data.decode("utf-8")
+    print(content)
+    return str(content) + "!!!"
+
+
+@app.route("/users", methods=['GET'], params=query_params)
+def users_list():
+    return jsonify(users)
+
+
+@app.route('/endpoint', methods=['DELETE'])
+def delete_endpoint():
+    return jsonify({'error': 'I\'m a teapot'}), 418
+
+
+@app.route('/change', methods=['PUT'])
+def change():
+    content = str(request.data.decode("utf-8"))+" input text"
+    print(content)
+    return jsonify(content)
+
 
 
 if __name__ == "__main__":
-
     app.run(debug=True)
